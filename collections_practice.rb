581f398b1369a316ffb0b20d2c64b50b9c0c574f
Richell Bonfield <richell.bonfield12@gmail.com>
2019-09-19 22:25:43 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -37,7 +37,7 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each_with_index.collect{|element, index|}
+  array.map.with_index.collect{|element, index|}
   if index != 1 
     element + "s"
   end
